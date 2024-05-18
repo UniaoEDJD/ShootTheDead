bc8c7153e24547ea3e32d9df32745dfa4934c7c2
Tiago Miranda <tmdmiranda@gmail.com>
2024-05-18 01:52:37 +0100

    ai

--- ShootTheDead/GameEntities/Enemy.cs
+++ ShootTheDead/GameEntities/Enemy.cs
@@ -11,9 +11,10 @@ namespace ShootTheDead.GameEntities
     public class Enemy : AnimatedSprite
     {
         public int HP { get; private set; }
-
+        private Rectangle rect {get; set; }
         public Enemy(Vector2 pos, Texture2D tex) : base(pos, tex)
         {
+            rect = new Rectangle(0, 0, 288, tex.Height);
             Speed = 100;
             HP = 2;
         }
@@ -33,6 +34,22 @@ namespace ShootTheDead.GameEntities
                 var dir = Vector2.Normalize(toPlayer);
                 sPosition += dir * Speed * Globals.deltaTime;
             }
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // Desenha o jogador com rota鈬o
+            spriteBatch.Draw(
+                sTexture,
+                sPosition,
+                rect,
+                Color.White,
+                Rotation,  // Aplica a rota鈬o
+                new Vector2(rect.Width / 2, rect.Height / 2),  // Origem da rota鈬o
+                1.0f,
+                SpriteEffects.None,
+                0f
+            );
+
         }
     }
 }
